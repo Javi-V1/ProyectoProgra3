7fc5e94748176feeca02eb589c1028c93f30f135
Javi-V1 <javiviba13@gmail.com>
2023-07-30 06:13:10 -0600

    Se completo lo que faltaba de pilas

--- ProyectoGrupo5/Capa_Logica/Pila/Orquestador_Pila.cs
+++ ProyectoGrupo5/Capa_Logica/Pila/Orquestador_Pila.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Capa_Modelo.Cola;
+using Capa_Modelo.Pila;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,74 @@ using System.Threading.Tasks;
 
 namespace Capa_Logica.Pila
 {
-    internal class Orquestador_Pila
+    public class Orquestador_Pila
     {
+        public NodoInt_Pila cabeza;
+
+
+        public bool Cabeza_No_Nula()
+        {
+
+            return cabeza != null;
+        }
+        public void Push(int _valor)
+        {
+
+            if (Cabeza_No_Nula())
+            {
+
+                NodoInt_Pila nodoNuevo = new NodoInt_Pila(_valor);
+
+                nodoNuevo.Siguiente = cabeza;
+                cabeza = nodoNuevo;
+
+            }
+            else
+            {
+
+                NodoInt_Pila nodoNuevo = new NodoInt_Pila(_valor);
+                cabeza = nodoNuevo;
+            }
+
+        }
+        public NodoInt_Pila Pop()
+        {
+
+            NodoInt_Pila nodoRetorno = null;
+
+            if (Cabeza_No_Nula())
+            {
+                //Nodo de referencia
+                NodoInt_Pila nodoActual = cabeza;
+                nodoRetorno = nodoActual;
+                nodoActual = nodoActual.Siguiente;
+                cabeza = nodoActual;
+            }
+
+            return nodoRetorno;
+
+        }
+
+        public void Imprimir_Pila()
+        {
+            Console.WriteLine("+-+- Empieza Pila-+-+");
+
+            if (Cabeza_No_Nula())
+            {
+                //Nodo referencia
+                NodoInt_Pila nodoActual = cabeza;
+
+                //Recorrer la lista
+                while (nodoActual != null)
+                {
+                    Console.WriteLine(nodoActual.Valor);
+
+                    nodoActual = nodoActual.Siguiente;
+                }
+
+            }
+            Console.WriteLine("+-+- Termina Pila-+-+");
+
+        }
     }
 }
--- ProyectoGrupo5/Capa_Modelo/Pila/NodoInt_Pila.cs
+++ ProyectoGrupo5/Capa_Modelo/Pila/NodoInt_Pila.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Capa_Modelo.Cola;
+using Capa_Modelo.Nodo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace Capa_Modelo.Pila
 {
-    internal class NodoInt_Pila
+    public class NodoInt_Pila : Nodo<int, NodoInt_Pila>
     {
+        public override int Valor { get; set; }
+        public override NodoInt_Pila Siguiente { get; set; }
+
+        public NodoInt_Pila(int _valor)
+        {
+            Valor = _valor;
+            Siguiente = null;
+        }
     }
 }
