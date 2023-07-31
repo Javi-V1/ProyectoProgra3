c06425fcd4d1854ba037ae1eddfae4166841f4af
Javi-V1 <javiviba13@gmail.com>
2023-07-31 04:06:05 -0600

    Se realizo un avance, aunque presenta errores

--- ProyectoGrupo5/Capa_Logica/Lista_Simple/Orquestador_Lista_Simple.cs
+++ ProyectoGrupo5/Capa_Logica/Lista_Simple/Orquestador_Lista_Simple.cs
@@ -1,4 +1,6 @@
 ﻿using Capa_Modelo.LS;
+using System.ComponentModel.Design;
+using System.Data.Common;
 
 namespace Capa_Logica.Lista_Simple
 {
@@ -6,6 +8,7 @@ namespace Capa_Logica.Lista_Simple
     {
 
         private NodoInt_LS cabeza;
+        private int tamanno=0;
 
         private bool Cabeza_No_Nula()
         {
@@ -27,6 +30,7 @@ namespace Capa_Logica.Lista_Simple
             if (!Cabeza_No_Nula())
             {
                 cabeza = nodoNuevo;
+                
             }
             else
             {
@@ -38,7 +42,9 @@ namespace Capa_Logica.Lista_Simple
                 }
 
                 nodoActual.Siguiente = nodoNuevo;
+                
             }
+            tamanno++;
         }
 
         public void Mostrar_Lista_Simple()
@@ -57,6 +63,87 @@ namespace Capa_Logica.Lista_Simple
                 }               
             }
             Console.WriteLine("*-*-*-*Termina Lista*-*-*-*");
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Ejercicio1_LS()
+        {
+            Llenar_LS();
+            Mostrar_Lista_Simple();
+            Bubble_Sort();
+            Mostrar_Lista_Simple();
+        }
+
+        public void Llenar_LS()
+        {
+            Random random = new Random();
+            for (int i = 0; i<5; i++)
+            {
+                Agregar_NodoInt_LS(random.Next(1, 31));
+            } 
+        }
+        public void Bubble_Sort()
+        {
+            NodoInt_LS nodoActual = cabeza;
+            Reordenar_Lista_Recursivo(nodoActual, nodoActual.Siguiente);
+
+        }
+        public void Reordenar_Lista_Recursivo(NodoInt_LS nodoActual, NodoInt_LS nodoSiguiente)
+        {
+            if (Lista_Ordenada())
+            {
+
+            }
+            else
+            {
+                int swap;
+                if (NodoSiguiente_Nulo(nodoSiguiente))
+                {
+                    nodoSiguiente = cabeza;
+                }
+
+                while(!NodoSiguiente_Nulo(nodoSiguiente) && Numero_Mayor_Siguiente(nodoActual, nodoSiguiente))
+                {
+                    Intercambiar_Valores(nodoActual, nodoSiguiente);
+                }
+
+                nodoActual = nodoSiguiente;
+                nodoSiguiente = nodoSiguiente.Siguiente;
+                Reordenar_Lista_Recursivo(nodoActual, nodoSiguiente);
+            }
+        }
+        public bool NodoSiguiente_Nulo(NodoInt_LS nodoSiguiente)
+        {
+            return (nodoSiguiente == null) ? true : false;
+        }
+
+        public bool Numero_Mayor_Siguiente(NodoInt_LS nodoActual, NodoInt_LS nodoSiguiente)
+        {
+            return (nodoActual.Valor > nodoSiguiente.Valor) ? true : false;
+        }
+
+        public void Intercambiar_Valores(NodoInt_LS nodoActual, NodoInt_LS nodoSiguiente)
+        {
+            int swap;
+            swap = nodoActual.Valor;
+            nodoActual.Valor = nodoSiguiente.Valor;
+            nodoSiguiente.Valor = swap;
+        }
+
+        public bool Lista_Ordenada()
+        {
+            NodoInt_LS nodoActual = cabeza;
+            while (nodoActual != null && nodoActual.Siguiente!=null)
+            {
+                if(nodoActual.Valor > nodoActual.Siguiente.Valor)
+                {
+                    return false;
+                }
+                nodoActual = nodoActual.Siguiente;
+            }
+            return true;
         }
     }
 }
--- ProyectoGrupo5/Capa_Presentacion/Program.cs
+++ ProyectoGrupo5/Capa_Presentacion/Program.cs
@@ -5,3 +5,5 @@ using Capa_Logica.Lista_Simple;
 Orquestador_Lista_Simple orquestador_LS = new Orquestador_Lista_Simple();
 Orquestador_Lista_Doble orquestador_LD = new Orquestador_Lista_Doble();
 Orquestador_Cola orquestador_Cola   = new Orquestador_Cola();
+
+orquestador_LS.Ejercicio1_LS();
