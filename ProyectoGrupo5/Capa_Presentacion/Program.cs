635b8f7f85bcaf4312f2f93120b7a322f6e6a362
Javi-V1 <javiviba13@gmail.com>
2023-07-31 16:31:00 -0600

    se establecio la estructura necesaria para realizar el radix sort

--- ProyectoGrupo5/Capa_Presentacion/Program.cs
+++ ProyectoGrupo5/Capa_Presentacion/Program.cs
@@ -4,8 +4,20 @@ using Capa_Logica.Lista_Simple;
 using Capa_Logica.Pila;
 
 Orquestador_Lista_Simple orquestador_LS = new Orquestador_Lista_Simple();
-Orquestador_Lista_Doble orquestador_LD = new Orquestador_Lista_Doble();
-Orquestador_Cola orquestador_Cola   = new Orquestador_Cola();
-Orquestador_Pila orquestador_Pila = new Orquestador_Pila();
+//Orquestador_Lista_Doble orquestador_LD = new Orquestador_Lista_Doble();
+//Orquestador_Cola orquestador_Cola   = new Orquestador_Cola();
+//Orquestador_Pila orquestador_Pila = new Orquestador_Pila();
 
 orquestador_LS.Ejercicio1_LS();
+
+Random random = new Random();
+int numero = random.Next(100, 501);
+int resultadoUnidades;
+int resultadoDecenas;
+int resultadoCentenas;
+
+resultadoCentenas = numero / 100; //esto me va a dar como resultado el numero en los centenares
+resultadoDecenas = numero % 100 / 10; //esto me va a dar como resultado el numero en los decenares
+resultadoUnidades = numero % 100 % 10; //esto me va a dar como resultado el numero en las unidades
+
+Console.WriteLine(numero + " centenas: " + resultadoCentenas + " decenas: " + resultadoDecenas + " unidades: " + resultadoUnidades);
