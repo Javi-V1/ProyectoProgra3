9c00461904719c4184575ea74a2bc76f6b9a4441
Javi-V1 <javiviba13@gmail.com>
2023-08-03 01:38:12 -0600

    se realizaron pruebas

--- ProyectoGrupo5/Capa_Logica/Cola/Orquestador_Cola.cs
+++ ProyectoGrupo5/Capa_Logica/Cola/Orquestador_Cola.cs
@@ -8,6 +8,7 @@ namespace Capa_Logica.Cola
     public class Orquestador_Cola
     {
         private NodoInt_Cola cabeza;
+        private NodoInt_Cola refCabeza;
         private NodoInt_Cola cola;
         private int length=0;
 
@@ -85,7 +86,7 @@ namespace Capa_Logica.Cola
             Llenar_Cola();
             Imprimir_Cola();
             Shell_Sort();
-            Imprimir_Cola();
+            //Imprimir_Cola();
         }
 
         public void Llenar_Cola()
@@ -95,50 +96,70 @@ namespace Capa_Logica.Cola
             {
                 Enqueue(random.Next(1,50));
             }
+            refCabeza = cabeza;
         }
 
         public void Shell_Sort()
         {
-            //prueba(cabeza);
-            Controlador(cabeza);
+            
+            Controlador(refCabeza);
         }
 
-        public void Controlador(NodoInt_Cola cabeza)
+        public void Controlador(NodoInt_Cola refCabeza)
         {
             int gap = length / 2;
-            while(gap > 0)
+            //gap /= 2;
+            //while (gap > 0)
             {
-                Recorrer_cola(cabeza, gap);
-                gap/=2;
+                Recorrer_cola(refCabeza, gap);
+                //gap /= 2;
             }
         }
 
         public void Recorrer_cola(NodoInt_Cola nodoActual, int gap)
         {
-            Console.WriteLine("gap : " + gap);
-
-
-            int valor1 = nodoActual.Valor;
-            for (int i = 0; i < gap; i++)
+            if(nodoActual.Siguiente!= null||!Lista_Ordenada())
             {
-                nodoActual = nodoActual.Siguiente;
-            }
-            int valor2 = nodoActual.Valor;
-
-            if (valor1 > valor2)
-            {
-                //Enqueue(valor2);
-                nodoActual.Valor = valor2;  
-
-                for (int i = 0; i < length; i++)
+                Console.WriteLine("Gap: " + gap);
+                int valor_1 = nodoActual.Valor;
+                for (int i = 0; i < gap; i++)
                 {
-                    Enqueue(Dequeue().Valor);
+                    nodoActual = nodoActual.Siguiente;
+                }
+                int valor_2 = nodoActual.Valor;
+
+                //Console.WriteLine("Valor 1: " + valor_1 + "/ Valor 2: " + valor_2);
+
+                //Se realiza el intercambio de valores
+                if (valor_1 > valor_2)
+                {
+                    nodoActual.Valor = valor_1;
+                    for (int i = gap; i > 0; i--)
+                    {
+                        nodoActual = nodoActual.Siguiente;
+                        Enqueue(Dequeue().Valor);
+                    }
+                    nodoActual.Valor = valor_2;
+                    refCabeza = nodoActual;
+                    Console.WriteLine("Se cambiaron valores");
+
+                    //Se vuelve al mismo formato de antes de la lista
+                    for (int i = gap; i > 0; i--)
+                    {
+                        nodoActual = nodoActual.Siguiente;
+                        Enqueue(Dequeue().Valor);
+                    }
                 }
 
-                // Enqueue(valor1);
-                nodoActual.Valor= valor1;
-                Console.WriteLine("Se cambiaron los valores " + valor1 + " por " + valor2);
                 Imprimir_Cola();
+                //Console.WriteLine(refCabeza.Valor);
+                nodoActual = refCabeza;
+                //Console.WriteLine(nodoActual.Valor);
+
+                nodoActual = nodoActual.Siguiente;
+                refCabeza = nodoActual;
+                //Console.WriteLine(nodoActual.Valor);
+                Recorrer_cola(nodoActual, gap);
             }
         }
 
